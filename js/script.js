ab7e178ac19bf178289553ff49475677fe41414a
Romashka <rroma13579@gmail.com>
2023-08-22 14:25:39 +0200

    need to fix animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 var Cards = document.querySelectorAll('.card');
 
-Cards.forEach(card=>{
-    card.addEventListener('click', () =>{
-        var cardClone = card.cloneNode(true);
-        cardClone.classList.add("cardClicked");
-        card.append(cardClone);
+Cards.forEach(card => {
+    card.addEventListener('click', () => {
+            for (i = 0; i < 4; i++) {
+                loop(i, card);
+            }
     });
 });
+  function loop(i, card){
+    setTimeout(function(){
+        var cardClone = card.querySelector('.card-image').cloneNode(true);
+        if (i % 2 == 0) {
+            cardClone.classList.add("cardClicked1");
+        }
+        else {
+            cardClone.classList.add("cardClicked2");
+        }
+        card.append(cardClone);
+      }, (750 * i-1));
+  }
